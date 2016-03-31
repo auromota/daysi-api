9052b54fcf77bcfd7fb093d551453f04101830c4
Auro Mota <palubo01@gmail.com>
2016-03-31 00:22:21 -0300

    Adding CORS

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 require('getmodule');
 
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+
 var users = getmodule('service/users');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cors());
 app.use(users);
 
 app.listen(process.env.PORT || 8080);
