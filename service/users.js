b6f568ae918541915977bb7d9e35150dcc4a0e7a
Auro Mota <palubo01@gmail.com>
2016-03-31 00:11:12 -0300

    testing api

--- service/users.js
+++ service/users.js
@@ -3,7 +3,14 @@ var users = express.Router();
 var dao = getmodule('./database/users');
 
 users.get('/users', function(req, res) {
-    dao.findAll(res);
+    db.get(function(err, connection) {
+        if(err) res.end();
+        connection.query('SELECT * from users', function(err, rows) {
+            connection.release();
+            if(err) res.end();
+            res.status(200).json(rows);
+        });
+    });
 });
 
 module.exports = users;
