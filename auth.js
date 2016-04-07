3e9a9c4e7bef8d2765d0405854832a9114e35624
Auro Mota <palubo01@gmail.com>
2016-04-07 16:35:12 -0300

    Adding authorization middleware

---
+++ auth.js
@@ -0,0 +1,22 @@
+var jwt = require('jsonwebtoken');
+var config = getmodule('config');
+
+var auth = {
+    isAuthorized : function(req, res, next) {
+        var token = req.body.token || req.query.token || req.headers['x-access-token'];
+        if(token) {
+            jwt.verify(token, config.jwt_secret, function(err, decoded) {
+                if(err) {
+                    res.status(403).json({type: false, message: 'Failed to authenticate token.'});
+                } else {
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+        } else {
+            res.status(403).json({type: false, message: 'No token provided.'});
+        }
+    }
+}
+
+module.exports = auth;
--- router.js
+++ router.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var userController = getmodule('controller/userController');
+var auth = getmodule('auth');
 
 router.route('/signup').post(userController.signUp);
 router.route('/signin').post(userController.signIn);
-router.route('/users').get(userController.getAll);
-router.route('/users/:userId').get(userController.getUser);
+router.route('/users').get(auth.isAuthorized, userController.getAll);
+router.route('/users/:userId').get(auth.isAuthorized, userController.getUser);
 
 module.exports = router;
